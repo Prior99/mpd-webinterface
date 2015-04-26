9f336c1d418875461d5ace1296d31e496ccd69fc
Frederick Gnodtke <frederick@gnodtke.net>
2015-04-26 16:09:49 +0200

    Rename file based on ID3-tags after upload.

--- htdocs/api.php
+++ htdocs/api.php
@@ -1,17 +1,40 @@
 <?php
+	require_once('getid3/getid3.php');
 	function upload() {
 		$files = $_FILES["upload"];
 		$array = Array();
 		if(isset($files)) {
-			var_dump($files);
 			$amount = count($files["name"]);
+			$okay = true;
 			for($i = 0; $i < $amount; $i++) {
-				$name = $files["name"][$i];
-				$tmp_name = $files["tmp_name"][$i];
-				move_uploaded_file($tmp_name, $GLOBALS["config"]["music"]."/".$name);
+				if(substr($files["type"][$i], 0, 5) !== "audio") {
+					$okay = false;
+					continue;
+				}
+				else {
+					$tmp_name = $files["tmp_name"][$i];
+					$name = $files["name"][$i];
+					$getID3 = new getID3();
+					$id3 = $getID3->analyze($tmp_name);
+					getid3_lib::CopyTagsToComments($id3);
+					$tag = $id3["comments"];
+					if(isset($tag) && isset($tag["artist"][0]) && isset($tag["title"][0])) {
+						$name = $tag["artist"][0]." - ".$tag["title"][0].substr($name, -4);
+						move_uploaded_file($tmp_name, $GLOBALS["config"]["music"]."/".$name);
+						echo("{okay:true}");
+					}
+					else {
+						$okay = false;
+					}
+				}
+			}
+			if(!$okay) {
+				echo('{okay:false,reason:"id3"}');
 			}
 		}
-		echo("true");
+		else {
+			echo('{okay:false,reason:"no_files"}');
+		}
 	}
 
 	require_once("src/mpd.php");
